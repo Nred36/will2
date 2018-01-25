dd576a3867d3dbcb02a3412c826283f0e6f9841e
Will <Will@10.0.1.30>
2018-01-25 00:24:57 -0500

    Messing around with the clean and build, moved all resources into src//resources

--- src/culm/will/Aircraft.java
+++ src/culm/will/Aircraft.java
@@ -29,7 +29,7 @@ public class Aircraft {
      * @param angle
      */
     public Aircraft(double angle) {
-        colour = "src//aircraft.png"; // set the colour as the default (purple)
+        colour = "src//resources//aircraft.png"; // set the colour as the default (purple)
         posistionX = angleInRadiansToPosistionX(angle); // set the initial horizontal posistion 
         posistionY = angleInRadiansToPosistionY(angle); // set the initial vertical posistion
         age = 0; // set the inital age of the aircraft to 0
